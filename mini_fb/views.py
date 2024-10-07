87b45c57f18d3e8d996fc55f129bf27cd3b9d827
Haider Khan <haiderk@bu.edu>
2024-10-06 21:51:55 -0400

    finished a5

--- mini_fb/views.py
+++ mini_fb/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView
-from .models import *
+from .models import Profile
 
 class ShowAllProfilesView(ListView):
     model = Profile
