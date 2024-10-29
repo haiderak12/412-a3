03c9e4b5187757ceba707aff041f5c6d2758c870
Haider Khan <haiderk@bu.edu>
2024-10-28 20:02:33 -0400

    assignment-8

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.db.models import Q
 
 # Create your models here.
 
@@ -26,6 +27,44 @@ class Profile(models.Model):
     
     def get_absolute_url(self):
         return reverse('show_profile', kwargs={'pk': self.pk})
+    
+    def get_friends(self):
+        friendships = Friend.objects.filter(Q(profile1=self) | Q(profile2=self))
+        friends = []
+        for friendship in friendships:
+            if friendship.profile1 == self:
+                friends.append(friendship.profile2)
+            else:
+                friends.append(friendship.profile1)
+        return friends
+    
+    def add_friend(self, other):
+        if self == other:
+            print("Cannot add yourself as a friend.")
+            return
+
+        friendship_exists = Friend.objects.filter(
+            (Q(profile1=self) & Q(profile2=other)) | (Q(profile1=other) & Q(profile2=self))
+        ).exists()
+
+        if not friendship_exists:
+            friendship = Friend(profile1=self, profile2=other)
+            friendship.save()
+            print(f"Friendship created between {self} and {other}.")
+        else:
+            print(f"Friendship already exists between {self} and {other}.")
+    
+    def get_friend_suggestions(self):
+        all_profiles = Profile.objects.exclude(pk=self.pk)
+        friends = self.get_friends()
+        suggestions = all_profiles.exclude(pk__in=[friend.pk for friend in friends])
+        return suggestions
+    
+    def get_news_feed(self):
+        friends = self.get_friends()
+        profiles = [self] + friends
+        news_feed = StatusMessage.objects.filter(profile__in=profiles).order_by('-timestamp')
+        return news_feed
     
 class StatusMessage(models.Model):
 
@@ -58,3 +97,19 @@ class Image(models.Model):
         Returns a string representation of the Image Instance
         '''
         return f"Image for StatusMessage {self.status_message.id} at {self.timestamp}"
+
+class Friend(models.Model):
+    profile1 = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='friend_set1'
+    )
+    profile2 = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='friend_set2'
+    )
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.profile1.firstname} {self.profile1.lastname} & {self.profile2.firstname} {self.profile2.lastname}"
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -5,6 +5,7 @@ from django.views.generic import *
 from .models import * # import the models
 from .forms import *
 from django.urls import reverse
+from django.shortcuts import redirect
 
 # class-based view
 class ShowAllProfilesView(ListView):
@@ -98,3 +99,38 @@ class UpdateStatusMessageView(UpdateView):
     def get_success_url(self):
         profile_pk = self.object.profile.pk
         return reverse('show_profile', kwargs={'pk': profile_pk})
+
+class CreateFriendView(View):
+    def dispatch(self, request, *args, **kwargs):
+        profile_pk = kwargs.get('pk')
+        other_pk = kwargs.get('other_pk')
+
+        try:
+            profile = Profile.objects.get(pk=profile_pk)
+            other_profile = Profile.objects.get(pk=other_pk)
+        except Profile.DoesNotExist:
+            return redirect('show_all_profiles')
+
+        profile.add_friend(other_profile)
+
+        return redirect(reverse('show_profile', kwargs={'pk': profile_pk}))
+
+class ShowFriendSuggestionsView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/friend_suggestions.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['suggestions'] = self.object.get_friend_suggestions()
+        return context
+
+class ShowNewsFeedView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/news_feed.html'
+    context_object_name = 'profile'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['news_feed'] = self.object.get_news_feed()
+        return context
